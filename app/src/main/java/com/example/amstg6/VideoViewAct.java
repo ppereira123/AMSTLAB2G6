00ed94cd48d90f3c0855431e710a957da53166b7
renatojaraor <57976123+renatojaraor@users.noreply.github.com>
2020-11-12 21:19:03 -0500

    nuevasActividades

---
+++ app/src/main/java/com/example/amstg6/VideoViewAct.java
@@ -0,0 +1,14 @@
+package com.example.amstg6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoViewAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_view);
+    }
+}
